b3f19a99e7378e42595441aafc6d21852a0684c3
Leperkhanh <jordannguyen880@gmail.com>
2021-03-03 14:11:29 -0500

    create render lists function

--- index.js
+++ index.js
@@ -7,35 +7,40 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const createListForm = document.querySelector(".list-form");
 
-  createListForm.addEventListener("submit", (e) => {
+  createListForm.addEventListener("submit", e => {
     createListHandler(e);
   });
 });
 
 function getLists() {
   fetch(listsEndPoint)
-    .then((resp) => resp.json())
-    .then((lists) => {
-      lists.data.forEach((list) => {
-        const listsContainer = document.querySelector(".lists-container");
-        const listDiv = document.createElement("div");
-        const listItem = document.createElement("h1");
-        const renderList = () => {
-          listItem.textContent = `${list.attributes.name}`;
-          listDiv.appendChild(listItem);
-          listsContainer.appendChild(listDiv);
-        };
-
-        renderList();
+    .then(resp => resp.json())
+    .then(lists => {
+      lists.data.forEach(list => {
+        render(list);
       });
     });
 }
 
+function render(list) {
+  const listsContainer = document.querySelector(".lists-container");
+  const listDiv = document.createElement("div");
+  const listItem = document.createElement("h1");
+
+  const renderList = () => {
+    listItem.textContent = `${list.attributes.name}`;
+    listDiv.appendChild(listItem);
+    listsContainer.appendChild(listDiv);
+  };
+
+  renderList();
+}
+
 function getTasks() {
   fetch(tasksEndPoint)
-    .then((resp) => resp.json())
-    .then((tasks) => {
-      tasks.data.forEach((task) => {
+    .then(resp => resp.json())
+    .then(tasks => {
+      tasks.data.forEach(task => {
         const tasksContainer = document.querySelector(".tasks-container");
         const taskDiv = document.createElement("div");
         const taskItem = document.createElement("h1");
@@ -65,9 +70,8 @@ function postList(name) {
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(bodyData),
   })
-    .then((resp) => resp.json())
-    .then((list) => {
-      console.log(list);
+    .then(resp => resp.json())
+    .then(list => {
       const listData = list.data;
       const listsContainer = document.querySelector(".lists-container");
       const listDiv = document.createElement("div");
