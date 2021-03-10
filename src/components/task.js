f83d7acdff3e1b18b9cdd926586d0fb5d7f20ba2
Leperkhanh <jordannguyen880@gmail.com>
2021-03-10 18:33:41 -0500

    add styling to task and list containers

--- src/components/task.js
+++ src/components/task.js
@@ -2,14 +2,17 @@ class Task {
   constructor(task) {
     this.id = task.id;
     this.body = task.body;
-    Task.all.push(this);
   }
 
   renderTask() {
     return `
-      <p>${this.body}</p>
+      <div class="flex text-right w-9/12 m-auto justify-between hover:bg-blue-700">
+      <p class="font-extrabold text-2xl mb-5">${this.body}</p>
+      <div class="justify-items-end">
+        <a class="view-list-button cursor-pointer">Edit</a>
+        <a class="delete-list-button cursor-pointer pl-5" id="delete-list-button">Delete</a>
+      </div>
+      </div>
     `;
   }
 }
-
-Task.all = [];
